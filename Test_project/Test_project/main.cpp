7367bfa09f168ae3319511dcda3a8449bf10bdd1
li <shmilhxp@163.com>
2018-05-24 01:34:21 +0800

    First commit

---
+++ Test_project/Test_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test_project
+//
+//  Created by Li on 2018/5/24.
+//  Copyright © 2018年 Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
