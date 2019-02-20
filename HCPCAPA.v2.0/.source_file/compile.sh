429d00da33c55f431885541e7994ae74ddb5ad83
li <shmilhxp@163.com>
2019-02-20 16:49:13 +0800

    20190220

--- HCPCAPA.v2.0/.source_file/compile.sh
+++ HCPCAPA.v2.0/.source_file/compile.sh
@@ -1,9 +1,14 @@
 
-echo '[INFO]copyping py to pyx'
-cp nndw.py nndw.pyx
-cp nnenv.py nnenv.pyx
-cp hcpcapacore.py hcpcapacore.pyx
 
+#!/bin/bash
+
+echo '[INFO]copyping py to pyx'
+
+ls | awk -F '\t' '{ if ( $1 ~ /.py$/ ) print $1}' | while read line
+do
+cp $line ${line%%.*}.pyx
+
+done
 
 echo '[INFO]building'
 python3 setup.py build_ext --inplace
