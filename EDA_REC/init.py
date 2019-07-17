1e58563c35fa66dfdb00e25f62f40aa4c8128f32
li <shmilhxp@163.com>
2019-07-17 18:52:25 +0800

    20190718

--- EDA_REC/init.py
+++ EDA_REC/init.py
@@ -1,2 +1,5 @@
 from eda_rec import app
-
+def app_factory(global_config, **local_config):
+    """This function wraps our simple WSGI app so it
+    can be used with paste.deploy"""
+    return app 
