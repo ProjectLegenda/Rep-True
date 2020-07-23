d82848eb07e92770f5d15037a35876ced9215fbc
li <shmilhxp@163.com>
2020-07-23 17:47:44 +0800

    minor update

--- easyetl/hcp_ts.py
+++ easyetl/hcp_ts.py
@@ -1,12 +1,12 @@
-from loader import load_intomysql
+from loader import load_intomysql,f_get_filelist
 
-TABLE_NAME='hcp_ts'
+table_name='hcp_ts'
 
-FILE_NAME_PREFIX = 'HCP_TS_'
+file_name_prefix = 'HCP_TS_'
 
 month_list = [20200709,20200629,20200521,20200430,20200330,20200227,20200102,20191226,20191128,20191031,20190926,20190901,20190801,20190627,20190601,20190429] 
 
 
 if __name__ == '__main__':
-
-    load_intomysql(TABLE_NAME,FILE_NAME_PREFIX,month_list)
+    file_list = f_get_filelist(file_name_prefix,month_list)
+    load_intomysql(table_name,file_list)
